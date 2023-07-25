f274b92e49493b6479d57d068ff171662244b1bf
saipavankommuri <saipavankommuri123@gmail.com>
2023-07-25 18:40:51 +0530

    secondary navbar

--- udemyclone/src/components/Top.js
+++ udemyclone/src/components/Top.js
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar";
 import "./Top.css"
+import NavbarSecondary from "./navbarsecondary/NavbarSecondary";
 const Top = () => {
     return (
         <div className="top-container">
            <Navbar/>
+           <NavbarSecondary/>
         </div>
     )
 }
---
+++ udemyclone/src/components/navbarsecondary/NavbarSecondary.js
@@ -0,0 +1,35 @@
+import "../Navbar.css"
+
+const NavbarSecondary = () => {
+    return (
+       
+     <div className="navbar-secondary">
+      <div className="navbar-element">
+        Development
+      </div>
+      <div className="navbar-element">Business</div>
+      <div className="navbar-element">
+        Finance & Accounting
+      </div>
+
+      <div className="navbar-element">IT & Software</div>
+      <div className="navbar-element">Office Productivity</div>
+      <div className="navbar-element">Personal Development</div>
+      <div className="navbar-element">
+       Design
+      </div>
+      <div className="navbar-element">
+        Marketing
+      </div>
+      <div className="navbar-element">
+        Health & Fitness
+      </div>
+      <div className="navbar-element">
+        Music
+      </div>
+    </div>
+       
+    )
+}
+
+export default NavbarSecondary;
