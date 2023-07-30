cf579342c8fa07af5473bd91c93561b282e865da
saipavankommuri <saipavankommuri123@gmail.com>
2023-07-30 14:44:57 +0530

    image carousel

--- udemyclone/src/components/Top.js
+++ udemyclone/src/components/Top.js
@@ -1,11 +1,16 @@
 import Navbar from "./Navbar";
 import "./Top.css"
+// import Carousel from "./carousel/Carousel";
+// import ImageSlider from "./imageslider/ImageSlider";
+// import ImageSlider2 from "./imageslider/ImageSlider2";
+import ImageCarousel from "./imagecarousel/ImageCarousel";
 import NavbarSecondary from "./navbarsecondary/NavbarSecondary";
 const Top = () => {
     return (
         <div className="top-container">
            <Navbar/>
            <NavbarSecondary/>
+          <ImageCarousel/>
         </div>
     )
 }
